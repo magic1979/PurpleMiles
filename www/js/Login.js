e296ebf243126640e84e025bd9f3c473ef6cb1dc
magic1979 <salvatore.bruni@gmail.com>
2016-09-24 02:14:39 +0200

    021424092016
    021424092016

--- www/js/Login.js
+++ www/js/Login.js
@@ -18,7 +18,7 @@ function onDeviceReady() {
 	
 	//cordova.plugins.diagnostic.isLocationEnabled(successCallback, errorCallback);
 	
-	/*cordova.plugins.locationAccuracy.canRequest(function(canRequest){
+	cordova.plugins.locationAccuracy.canRequest(function(canRequest){
 		if(canRequest){
 			cordova.plugins.locationAccuracy.request(function (success){
 			//alert("Successfully requested accuracy: "+success.message);
@@ -31,7 +31,7 @@ function onDeviceReady() {
 				}
 			}, cordova.plugins.locationAccuracy.REQUEST_PRIORITY_HIGH_ACCURACY);
 		}
-	});*/
+	});
 	
 	
 
